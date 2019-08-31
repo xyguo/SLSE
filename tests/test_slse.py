e66da351ae76d246d991e1c16d03d765e6429cc6
xyguo_mac <xyguo.ubd@gmail.com>
2019-08-31 17:23:45 -0400

    add tests for SLSE over mixed link function; and tests for the fallback behavior of _finding_root

--- tests/test_slse.py
+++ tests/test_slse.py
@@ -6,7 +6,7 @@ from numpy.testing import assert_almost_equal, assert_array_almost_equal
 from SLSE.slse import (
     SLSE, _sub_sampling_cov, _finding_root
 )
-from utils.link_funcs import PolynLink
+from utils.link_funcs import PolynLink, LogisticLink, LogexpLink
 from utils.data_gen import gen_slcnr_obj
 
 
@@ -28,31 +28,71 @@ class TestUtils(unittest.TestCase):
         r = _finding_root(func, x0=-3, method='brentq')
         assert_almost_equal(r, -1.879385233, decimal=4)
 
-        # test if the function fails when there's no root
-        bad_func = lambda x: x ** 2 + 2 * x + 2
-        bad_fprime = lambda x: 2 * x + 2
-        try:
+        # test if the function falls back to brute force when there's no root
+        bad_func = lambda x: -(x ** 2 + 2 * x + 2)
+        bad_fprime = lambda x: -(2 * x + 2)
+        with warnings.catch_warnings(record=True) as w:
+            # Cause all warnings to always be triggered.
+            warnings.simplefilter("always")
+
+            # Call some code that triggers a custom warning.
             r = _finding_root(bad_func, x0=-2, fprime=bad_fprime)
-        except ValueError:
-            pass
-        else:
-            assert False
+
+            # ignore any non-custom warnings that may be in the list
+            w = list(filter(lambda i: issubclass(i.category, UserWarning), w))
+
+            assert len(w) == 2
+            assert 'gridsearch' in str(w[-1].message)
 
     def test_SLSE(self):
         # recall
         # y = \sum^k_{i=1} z_i f_i(\langle \beta^*_i, x \rangle) + \epsilon
-        F = [PolynLink(degree=1),
-             PolynLink(degree=1, coeff=2),
-             PolynLink(degree=3)]
-        true_beta = np.array(
-            [[1, 1],
-             [0.5, 2],
-             [2, 0.5]]
-        )
-        X = np.random.normal(loc=2, scale=5, size=(10, 2))
-        Z = np.random.randn(10, 3)
-        Y = gen_slcnr_obj(X, true_beta, F, Z, eps_std=0.2)
+        n_samples, n_features = 200000, 5
+        n_links = 3
+        true_beta = np.random.normal(1, 4, size=(n_links, n_features))
+        X = np.random.uniform(low=-1/n_features, high=1/n_features,
+                              size=(n_samples, n_features))
+        Z = np.random.randn(n_samples, n_links)
 
         model = SLSE()
-        model.fit(X, Y, Z, F)
-        assert_array_almost_equal(true_beta, model.estimator, decimal=2)
+
+        # test polynomial link func
+        F1 = [PolynLink(degree=1) for i in range(n_links)]
+        Y = gen_slcnr_obj(X, true_beta, F1, Z, eps_std=0.1)
+        model.fit(X, Y, Z, F1)
+        rel_err = model.loss(true_beta) / np.linalg.norm(true_beta, axis=1)
+        assert np.all(rel_err < 0.2)
+
+        rel_err1 = model.loss(true_beta, relative=True)
+        assert_array_almost_equal(rel_err, rel_err1)
+
+        # test logistic link func
+        F2 = [LogisticLink() for i in range(n_links)]
+        Y = gen_slcnr_obj(X, true_beta, F2, Z, eps_std=0.1)
+        model.fit(X, Y, Z, F2)
+        rel_err = model.loss(true_beta) / np.linalg.norm(true_beta, axis=1)
+        assert np.all(rel_err < 0.2)
+
+        rel_err1 = model.loss(true_beta, relative=True)
+        assert_array_almost_equal(rel_err, rel_err1)
+
+        # test logexp link func
+        F3 = [LogexpLink() for i in range(n_links)]
+        Y = gen_slcnr_obj(X, true_beta, F3, Z, eps_std=0.1)
+        model.fit(X, Y, Z, F3)
+        rel_err = model.loss(true_beta) / np.linalg.norm(true_beta, axis=1)
+        assert np.all(rel_err < 0.2)
+
+        rel_err1 = model.loss(true_beta, relative=True)
+        assert_array_almost_equal(rel_err, rel_err1)
+
+        # test mixed link func
+        F4 = [PolynLink(degree=3), LogexpLink(), LogisticLink()]
+        Y = gen_slcnr_obj(X, true_beta, F4, Z, eps_std=0.01)
+        model.fit(X, Y, Z, F4)
+        rel_err = model.loss(true_beta) / np.linalg.norm(true_beta, axis=1)
+        assert np.all(rel_err < 0.2)
+
+        rel_err1 = model.loss(true_beta, relative=True)
+        assert_array_almost_equal(rel_err, rel_err1)
+
